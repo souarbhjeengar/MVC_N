c0dc0143e41416263bd608ef19c6fa3a42da151d
souarbh <souarbhrahtore50@gmail.com>
2021-12-02 22:10:33 +0530

    commit index reloded

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<php
-$helperfiles=glob("helper/*.php");
-array_map(function($filename){
-  include_once $filename;
-},$helperfiles);
-spl_autoload_register();
-  print_r($_GET);  require_once "apps/libs/Autoload.php"
-  $obj=new Autoload();
-
+<?php
+spl_autoload_register(function ($clsname){
+    $path="apps/libs/$clsname.php";
+    if(file_exists($path)){
+        include_once $path;
+    }
+})
 ?>
